fcf2a9723be2be4fbea7e77acb59e2a61e4deb47
KevinBlom <KevinBlom@users.noreply.github.com>
2020-12-03 20:09:24 +0100

    Update Day 03 1.py

---
+++ Day 03 1.py
@@ -0,0 +1,49 @@
+from tools import *
+import math
+lines = readFileToStrings("Input03")
+
+space = "."
+tree = "#"
+
+slopes = [[1, 1],
+          [3, 1],
+          [5, 1],
+          [7, 1],
+          [1, 2]]
+
+arrayOfTreecount = []
+
+
+def evaluatePosition(char):
+    if char == space:
+        return 0
+    elif char == tree:
+        return 1
+    else:
+        return -10000
+
+
+def runWithSlope(horizontalStep, verticalStep):
+    countOfTrees = 0
+    xPos = 0
+    yPos = 0
+    for line in lines:
+
+        if (yPos % verticalStep != 0):
+            yPos += 1
+            continue
+
+        countOfTrees += evaluatePosition(line[xPos])
+
+        xPos += horizontalStep
+        yPos += 1
+        if xPos >= (len(line) - 1):
+            xPos = xPos % (len(line) - 1)
+
+    return countOfTrees
+
+
+for slope in slopes:
+    arrayOfTreecount.append(runWithSlope(slope[0], slope[1]))
+
+print(math.prod(arrayOfTreecount))
