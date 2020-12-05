ebd991afacdf07b4b5aa0483471c4c43c119433c
KevinBlom <KevinBlom@users.noreply.github.com>
2020-12-05 15:51:32 +0100

    Comments

--- Day 05 1.py
+++ Day 05 1.py
@@ -8,9 +8,11 @@ highestSeatID = 0
 
 for boardingPass in boardingPasses:
 
+    # Convert input to binary
     rowData = boardingPass[:7].replace('F', '0').replace('B', '1')
     columnData = boardingPass[7:].replace('L', '0').replace('R', '1')
 
+    # Convert binary to integer
     row = int(rowData, 2)
     column = int(columnData, 2)
 
--- Day 05 2.py
+++ Day 05 2.py
@@ -7,9 +7,11 @@ seatIDs = []
 
 for boardingPass in boardingPasses:
 
+    # Convert input to binary
     rowData = boardingPass[:7].replace('F', '0').replace('B', '1')
     columnData = boardingPass[7:].replace('L', '0').replace('R', '1')
 
+    # Convert binary to integer
     row = int(rowData, 2)
     column = int(columnData, 2)
 
@@ -18,6 +20,7 @@ for boardingPass in boardingPasses:
 
 seatIDs.sort()
 walkerID = seatIDs[0]
+
 for seatID in seatIDs:
     if seatID != walkerID:
         print(f"Your seatnumber is {walkerID}")
